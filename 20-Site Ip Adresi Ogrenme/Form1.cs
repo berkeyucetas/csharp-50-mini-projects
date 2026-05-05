eec8122c430f8992f77cf5ad1a68d9f5ccf7899b
berkeyucetas <116177776+berkeyucetas@users.noreply.github.com>
2026-05-05 12:55:29 +0300

    Add files via upload

---
+++ 20-Site Ip Adresi Ogrenme/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Site_Ip_Adresi_Ögrenme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBilgi_Click(object sender, EventArgs e)
+        {
+            textBilgisayar.Text = "Bilgisayar: " + Dns.GetHostName();
+            foreach (IPAddress adres in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                textAdres.Text = "Ip Adresi: "+ adres;
+            }
+        }
+    }
+}
